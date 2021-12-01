f373e301b95e5387724b77e4898a2f99946f2bc8
Murat Okutucu <54741414+muratokutucu@users.noreply.github.com>
2021-12-01 16:02:44 +0100

    Add AdditionTest.java

---
+++ AdditionTest.java
@@ -0,0 +1,12 @@
+
+public class AdditionTest {
+
+	public static void AddTest(){
+		assert Addition.Add(1, 1) == 2 : "Should be equal to 2"; // la condition doit être ce qui doit être correct
+	}
+
+	public static void main(String[] args){
+		AddTest();
+	}
+
+}
